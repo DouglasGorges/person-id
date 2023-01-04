4e03f55a7814fd77f2b18d340375e7bda6a67ceb
Douglas Gorges <douglasgorges@Douglass-MacBook-Air.local>
2023-01-04 11:52:53 -0300

    Added static page v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'person-id';
+idade = '1 ano, 6 meses';
+
+  
 }
